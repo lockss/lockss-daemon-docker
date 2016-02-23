77b82b670127ae3bb1697b37006d3727665361a6
dlvargas <dlvargas@stanford.edu>
2016-02-22 18:11:33 -0800

    Changed to explict port mapping

--- start.sh
+++ start.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 docker run \
-    -itP \
+    -it \
     --rm \
     -v `pwd`/cache:/cache \
     -v `pwd`/log/lockss:/var/log/lockss \
     -v `pwd`/lockss:/opt/lockss \
+    -p 9729:9729 \
+    -p 8080-8086:8080-8086 \
     lockss/lockss-daemon
