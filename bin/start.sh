cdeba2d4436e26866ef0c6fbd7dc5636bd4a24b1
Rinocenc <rinocenc@stanford.edu>
2026-03-12 15:40:26 -0500

    Update start.sh
    Mapped /etc/lockss instead of /etc/lockss/config.dat

--- bin/start.sh
+++ bin/start.sh
@@ -5,7 +5,7 @@ docker run \
     -n lockss \
     -p 9729:9729 \
     -p 8081:8081 \
-    -v /etc/lockss/config.dat:/etc/lockss/config.dat \
+    -v /etc/lockss:/etc/lockss \
     -v /lockss:/lockss \
     -v /var/log/lockss:/var/log/lockss \
     -v /etc/localtime:/etc/localtime:ro \
